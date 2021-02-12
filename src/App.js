2b6b792e57da5716cc0e425a6b3680ea0514624a
Uduak Essien <acushla4real@gmail.com>
2021-02-12 10:20:27 +0100

    Update page with top nav, main nav and slider

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import './App.css';
+import React from 'react';
+import HomeSlider from './components/HomeSlider';
+import MainNav from './components/mainNav';
+import TopNav from './components/topNav';
 
 function App() {
   return (
     <div>
+      <TopNav />
+      <MainNav />
+      <HomeSlider />
       <h1>Hello VKS</h1>
     </div>
   );
