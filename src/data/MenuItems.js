2f51006b8d937f27b980826bafabac0628de6f12
Uduak Essien <acushla4real@gmail.com>
2021-02-16 17:20:03 +0100

    Add topMenu data item

---
+++ src/data/MenuItems.js
@@ -0,0 +1,33 @@
+// eslint-disable-next-line import/prefer-default-export
+export const topMenuLinks = [
+  {
+    id: 1,
+    title: 'Career',
+    url: '#career',
+    extra_class: 'pr-3',
+  },
+  {
+    id: 2,
+    title: 'Terms of Use',
+    url: '#terms',
+    extra_class: 'pr-3',
+  },
+  {
+    id: 3,
+    title: 'Policy',
+    url: '#policy',
+    extra_class: 'pr-3',
+  },
+  {
+    id: 4,
+    title: 'News',
+    url: '#news',
+    extra_class: 'pr-3',
+  },
+  {
+    id: 5,
+    title: 'Contact Us',
+    url: '#contact',
+    extra_class: '',
+  },
+];
