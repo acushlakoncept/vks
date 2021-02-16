e211e6b63f1a25156b5192151ad113da53654b33
Uduak Essien <acushla4real@gmail.com>
2021-02-16 16:20:28 +0100

    Add projects component

---
+++ src/components/Projects.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+function Projects() {
+  return (
+    <Container>
+      <h2>Latest Projects</h2>
+      <p>
+        VKS has achieved a wide reference list by successful undertakings on
+        turn – key basis in the diversified fields such as residential,
+        commercial, industrial buildings, highway construction and infrastructure
+        works, complete factories of various sorts as well as waste water treatment plants.
+      </p>
+      <div className="projects-thumbnails">
+        <div>Project Box</div>
+      </div>
+    </Container>
+  );
+}
+
+export default Projects;
