979d88bd8f7454df27283f520f35566a1d2d2da1
Uduak Essien <acushla4real@gmail.com>
2021-02-18 10:10:45 +0100

    Add react intersection observer

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { motion } from 'framer-motion';
+import FadeInWhenVisible from './FadeInWhenVisible';
 
 export default function Feature({ image, title, desc }) {
   return (
-    <div className="feature-item">
-      <img src={image} alt={title} />
-      <h2 className="title">{title}</h2>
-      <p className="desc">{desc}</p>
-    </div>
+    <FadeInWhenVisible>
+      <motion.div
+        whileHover={{
+          scale: 1.1,
+        }}
+        className="feature-item"
+      >
+        <motion.img
+          whileHover={{
+            scale: 1.1,
+          }}
+          transition={{ type: 'spring', stiffness: 300 }}
+          className="feature-item__img"
+          src={image}
+          alt={title}
+        />
+        <h2 className="title pt-2">{title}</h2>
+        <p className="desc">{desc}</p>
+      </motion.div>
+    </FadeInWhenVisible>
   );
 }
 
